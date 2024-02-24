50fda4df12cc99c0a0a1aa8f45ac68350d9fca8b
Amruthamsh A <32568328+Amruthamsh@users.noreply.github.com>
2024-02-24 12:28:18 +0530

    book title into quiz

--- components/ImageToQuiz.js
+++ components/ImageToQuiz.js
@@ -9,6 +9,8 @@ import {
   Platform,
   StatusBar,
   ScrollView,
+  KeyboardAvoidingView,
+  TextInput,
 } from "react-native";
 import React, { useState } from "react";
 import axios from "axios";
@@ -25,6 +27,8 @@ export default function ImageToQuiz() {
 
   const [imageUri, setImageUri] = useState(null);
   const [textOutput, setOutput] = useState("");
+  const [bookName, setBookName] = useState("");
+  const [bookConfirmed, confirmBook] = useState(false);
 
   const navigation = useNavigation();
 
@@ -46,7 +50,8 @@ export default function ImageToQuiz() {
 
   const camImage = async () => {
     try {
-      const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+      const permissionResult =
+        await ImagePicker.requestCameraPermissionsAsync();
 
       if (permissionResult.granted === false) {
         alert("You've refused to allow this appp to access your camera!");
@@ -103,7 +108,7 @@ export default function ImageToQuiz() {
     setLoading(false);
   };
 
-  const generateQuiz = async () => {
+  const generateQuizFromImage = async () => {
     try {
       setLoading(true);
       // For text-only input, use the gemini-pro model
@@ -127,7 +132,53 @@ export default function ImageToQuiz() {
         }`;
 
       const result = await model.generateContent([textOutput, prompt]);
-      const response = await result.response;
+      const response = result.response;
+      const text = response.text();
+
+      let startIndex = text.indexOf("{");
+      let endIndex = text.lastIndexOf("}");
+      if (startIndex !== -1 && endIndex !== -1) {
+        let jsonString = text.substring(startIndex, endIndex + 1);
+        navigation.navigate("QuizScreen", { jsonString });
+      } else {
+        console.log("No JSON object found within curly braces");
+        setOutput(text);
+      }
+
+      setLoading(false);
+    } catch (error) {
+      console.error("Error creating quiz: ", error);
+      alert("Error generating Quiz from gemini");
+    }
+  };
+
+  const generateQuizFromBookName = async () => {
+    try {
+      setLoading(true);
+      // For text-only input, use the gemini-pro model
+      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+      const prompt = `Generate 5 to 10 multiple choice quiz from the book ${bookName} such that questions are thought-provoking. 
+      Let the output be in JSON format in the following structure:
+        {
+        "output": [
+        {
+        "question": ...,
+        "options": [],
+        "answer": index of options
+        },
+        {
+        "question": ...,
+        "options": [],
+        "answer": index of options
+        }
+        ]
+        }`;
+
+      console.log(prompt);
+
+      const result = await model.generateContent([prompt]);
+      const response = result.response;
       const text = response.text();
 
       let startIndex = text.indexOf("{");
@@ -150,45 +201,90 @@ export default function ImageToQuiz() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Reading Adventures</Text>
-      <ScrollView>
-        {imageUri && (
-          <Image
-            source={{ uri: imageUri }}
-            style={{ width: Dimensions.get("window").width - 30, height: 300 }}
-            resizeMode="contain"
-          />
-        )}
-        <TouchableOpacity onPress={pickImage} style={styles.button}>
-          <Text style={styles.text}>Choose an Image from gallery</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={camImage} style={styles.button}>
-          <Text style={styles.text}>Choose an Image from Camera</Text>
-        </TouchableOpacity>
 
-        {imageUri && (
-          <TouchableOpacity
-            disabled={loading} // Disable when loading is true
-            onPress={analyzeImage}
-            style={styles.button}
-          >
-            <Text style={styles.text}>Analyze</Text>
+      {imageUri && (
+        <Image
+          source={{ uri: imageUri }}
+          style={{
+            width: Dimensions.get("window").width - 30,
+            height: 300,
+          }}
+          resizeMode="contain"
+        />
+      )}
+
+      {!bookConfirmed && (
+        <>
+          <TouchableOpacity onPress={pickImage} style={styles.button}>
+            <Text style={styles.text}>Choose an Image from gallery</Text>
           </TouchableOpacity>
-        )}
-
-        {textOutput && (
-          <TouchableOpacity
-            disabled={loading} // Disable when loading is true
-            onPress={generateQuiz}
-            style={styles.button}
-          >
-            <Text style={styles.text}>Generate Quiz</Text>
+          <TouchableOpacity onPress={camImage} style={styles.button}>
+            <Text style={styles.text}>Choose an Image from Camera</Text>
           </TouchableOpacity>
-        )}
+        </>
+      )}
 
-        {loading && (
-          <ActivityIndicator size="medium" color="#0000ff" /> // Activity indicator or custom loading text
-        )}
-      </ScrollView>
+      {imageUri && (
+        <TouchableOpacity
+          disabled={loading} // Disable when loading is true
+          onPress={analyzeImage}
+          style={styles.button}
+        >
+          <Text style={styles.text}>Analyze</Text>
+        </TouchableOpacity>
+      )}
+
+      {textOutput && (
+        <TouchableOpacity
+          disabled={loading} // Disable when loading is true
+          onPress={generateQuizFromImage}
+          style={styles.button}
+        >
+          <Text style={styles.text}>Generate Quiz</Text>
+        </TouchableOpacity>
+      )}
+
+      {!imageUri && (
+        <View>
+          {!bookConfirmed && <Text style={styles.text}>or</Text>}
+          <TextInput
+            editable={!bookConfirmed}
+            value={bookName}
+            style={styles.button}
+            placeholder="Enter a Book Name"
+            onChangeText={(text) => setBookName(text)}
+          ></TextInput>
+
+          {bookConfirmed ? (
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => confirmBook(false)}
+            >
+              <Text style={styles.text}>Change Book</Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => confirmBook(true)}
+            >
+              <Text style={styles.text}>Confirm Book</Text>
+            </TouchableOpacity>
+          )}
+
+          {bookConfirmed && (
+            <TouchableOpacity
+              style={styles.button}
+              onPress={generateQuizFromBookName}
+            >
+              <Text style={styles.text}>Generate Quiz on the Book</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      )}
+
+      {loading && (
+        <ActivityIndicator size="medium" color="#0000ff" /> // Activity indicator or custom loading text
+      )}
     </View>
   );
 }
@@ -208,7 +304,7 @@ const styles = StyleSheet.create({
     color: "brown",
   },
   button: {
-    marginHorizontal: 30,
+    width: 275,
     backgroundColor: "white",
     borderRadius: 10,
     padding: 10,
--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -2,7 +2,8 @@ import {
   View,
   Text,
   SafeAreaView,
-  TouchableOpacity,Platform,
+  TouchableOpacity,
+  Platform,
   Alert,
 } from "react-native";
 import { StyleSheet } from "react-native";
@@ -85,7 +86,7 @@ export default function QuizScreen({ route, navigation }) {
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const currentQuestion = questions.output[currentQuestionIndex];
-  const [hasFinishedQuiz,setFinishedQuiz] = useState(false);
+  const [hasFinishedQuiz, setFinishedQuiz] = useState(false);
 
   const [selectedOptions, setSelectedOptions] = useState(
     Array(questions.output.length).fill(null)
@@ -141,7 +142,13 @@ export default function QuizScreen({ route, navigation }) {
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, padding: 20, marginHorizontal: Platform.OS === "ios" ? 16 : 0 }}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        padding: 20,
+        marginHorizontal: Platform.OS === "ios" ? 16 : 0,
+      }}
+    >
       <Text style={{ fontSize: 30, marginVertical: 20 }}>Quiz</Text>
       <Text style={{ fontSize: 18, marginBottom: 10 }}>
         {currentQuestionIndex + 1}. {currentQuestion.question}
@@ -152,8 +159,16 @@ export default function QuizScreen({ route, navigation }) {
           disabled={hasFinishedQuiz}
           onPress={() => handleOptionSelect(optionIndex)}
           style={{
-            backgroundColor: hasFinishedQuiz? (optionIndex === currentQuestion.answer ? "lightgreen" : selectedOptions[currentQuestionIndex] === optionIndex ? "red":"white"): selectedOptions[currentQuestionIndex] === optionIndex ? "#6369D1":"white"
-            ,padding: 10,
+            backgroundColor: hasFinishedQuiz
+              ? optionIndex === currentQuestion.answer
+                ? "lightgreen"
+                : selectedOptions[currentQuestionIndex] === optionIndex
+                ? "red"
+                : "white"
+              : selectedOptions[currentQuestionIndex] === optionIndex
+              ? "#6369D1"
+              : "white",
+            padding: 10,
             marginTop: 5,
           }}
         >
@@ -202,7 +217,22 @@ export default function QuizScreen({ route, navigation }) {
         </TouchableOpacity>
       </View>
 
-      {selectedOptions.every((option) => option !== null) && !hasFinishedQuiz && (
+      {selectedOptions.every((option) => option !== null) &&
+        !hasFinishedQuiz && (
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              margin: 20,
+            }}
+          >
+            <TouchableOpacity onPress={handleSubmit}>
+              <Text style={{ fontSize: 20 }}>Submit!</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+
+      {hasFinishedQuiz && (
         <View
           style={{
             flex: 1,
@@ -210,23 +240,11 @@ export default function QuizScreen({ route, navigation }) {
             margin: 20,
           }}
         >
-          <TouchableOpacity onPress={handleSubmit}>
-            <Text style={{ fontSize: 20 }}>Submit!</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Quests")}>
+            <Text style={{ fontSize: 20 }}>Return to quests!</Text>
           </TouchableOpacity>
         </View>
       )}
-
-      {hasFinishedQuiz && (<View
-          style={{
-            flex: 1,
-            alignItems: "center",
-            margin: 20,
-          }}
-        >
-          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
-            <Text style={{ fontSize: 20 }}>Return to Quests!</Text>
-          </TouchableOpacity>
-        </View>)}
     </SafeAreaView>
   );
 }
